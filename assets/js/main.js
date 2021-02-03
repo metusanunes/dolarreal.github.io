41c4e9b47737a1d22ec42f300809632b66400d39
metusanunes <metusanunes@hotmail.com>
2021-02-03 20:56:58 -0300

    aa
    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-alert('~~');
+const a = function() {
+    const as = ['a','b','c'];
+    for (let ass of as) {
+        const aa= document.createElement('div')
+        container.appenchild(aa)
+        aa.id = ass
+        console.log(aa)
+    }
+}
